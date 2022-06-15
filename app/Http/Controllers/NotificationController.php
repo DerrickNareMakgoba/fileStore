7b5453f22205bbb0463ed5cc3b21916e030b502f
makgobad <makgobad@invokesolutions.co.za>
2022-06-15 13:52:51 +0200

    added service for notifications

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,26 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\NotificationService;
 use Illuminate\Http\Response;
 
 class NotificationController extends Controller
 {
-    public function SendSMS(): Response
+    public function SendSMS(NotificationService $service): Response
     {
-        $basic  = new \Vonage\Client\Credentials\Basic("b2c70b0a", "ffieqlLFJOySfO4j");
-        $client = new \Vonage\Client($basic);
-
-        $response = $client->sms()->send(
-            new \Vonage\SMS\Message\SMS("27732293080", 'test', 'HELLO USER')
-        );
-
-        $message = $response->current();
-
-        if ($message->getStatus() == 0) {
-            echo "The message was sent successfully\n";
-        } else {
-            echo "The message failed with status: " . $message->getStatus() . "\n";
-        }
+        $service->sendSMS();
         return $this->jsonSuccess();
     }
 }
