6cf97d69a66feeb5e584898245a69fd255faefbb
makgobad <makgobad@invokesolutions.co.za>
2022-06-15 13:39:30 +0200

    added api logic to send sms

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,6 +8,20 @@ class NotificationController extends Controller
 {
     public function SendSMS(): Response
     {
+        $basic  = new \Vonage\Client\Credentials\Basic("b2c70b0a", "ffieqlLFJOySfO4j");
+        $client = new \Vonage\Client($basic);
+
+        $response = $client->sms()->send(
+            new \Vonage\SMS\Message\SMS("27732293080", 'test', 'HELLO USER')
+        );
+
+        $message = $response->current();
+
+        if ($message->getStatus() == 0) {
+            echo "The message was sent successfully\n";
+        } else {
+            echo "The message failed with status: " . $message->getStatus() . "\n";
+        }
         return $this->jsonSuccess();
     }
 }
